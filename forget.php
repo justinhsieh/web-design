a7ccdc4fd4efdc399ccb75ea7474a4b9455eaf62
Justin <124439574+justinhsieh@users.noreply.github.com>
2025-05-01 22:06:43 +0800

    Update forget.php

--- forget.php
+++ forget.php
@@ -83,8 +83,8 @@
         </a>
         <nav aria-label="breadcrumb" class="ms-auto me-3">
           <ol class="breadcrumb mb-0">
-            <li class="breadcrumb-item"><a href="login.html" class="link-underline link-underline-opacity-0">登入</a></li>
-            <li class="breadcrumb-item"><a href="register.html" class="link-underline link-underline-opacity-0">註冊</a></li>
+            <li class="breadcrumb-item"><a href="login.php" class="link-underline link-underline-opacity-0">登入</a></li>
+            <li class="breadcrumb-item"><a href="register.php" class="link-underline link-underline-opacity-0">註冊</a></li>
           </ol>
         </nav>
         <div class="tool-right me-4 d-flex">
@@ -129,50 +129,6 @@
         </div>
     </section>
    </main>
-  <!-- 頁尾 -->
-  <footer class="bg-dark">
-    <div class="container-fluid">
-      <div class="row bg-dark py-5 align-items-center">
-        <div class="col-12 col-md-6 text-light d-flex justify-content-center mt-3">
-          <span class="d-flex w-75 border border-2 border-white rounded">防詐騙提醒：3C用品店絕不會以電話或簡訊通知訂單/分期出錯、或變更付款方式，更不會要您前往ATM進行任何操作！不應在3C用品店以外的任何地方輸入3C用品店帳密(例如非政府官方的電子發票app)，以免權益受損！</span>
-        </div>
-        <div class="col-12 col-md-6 d-flex mt-3">
-          <div class="col-7 d-flex ms-3">
-            <div class="subscribe w-75 ms-auto">
-              <div class="text-light fs-5 mb-2">Email:</div>
-              <form action="" id="subscribe" class="d-flex">
-                <input type="email" class="form-control w-75" id="email" name="email2" placeholder="輸入E-mail" aria-label="Recipient's username" aria-describedby="button-addon2">
-                <button class="btn btn-outline-secondary text-light" type="submit" id="button-addon2">訂閱</button>
-              </form>
-              <span id="error-container"></span>
-            </div>
-          </div>
-          <div class="col-5">
-            <div class="followus">
-              <div class="title text-light text-center followus_title">FOLLOW US ON</div>
-              <div class="d-flex justify-content-center gap-3 mt-3">
-                <a href="#" title="facebook">
-                  <i class="fa-brands fa-square-facebook"></i>
-                </a>
-                <a href="#" title="Instagram">
-                  <i class="fa-brands fa-instagram"></i>
-                </a>
-                <a href="#"title="Line">
-                  <i class="fa-brands fa-line"></i>
-                </a>
-              </div>
-            </div>
-          </div>          
-        </div>
-      </div>
-    </div>
-    <div class="container text-center">
-      <div class="row">
-        <div class="col">
-          <p class="copyright text-light">COPYRIGHT@3C用品店 CO.,LTD.ALL RIGHTS RESERVED.</p>
-        </div>
-      </div>
-    </div>
-  </footer>
+   <?php include'footer.php'; ?>
 </body>
 </html>
